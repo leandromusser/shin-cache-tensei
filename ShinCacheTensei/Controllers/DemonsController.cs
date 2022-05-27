6f30e0c0c3d1c6a7ae45efa7c7572d6b5d09c0d1
Leandro Rocha <leandro.musser@gmail.com>
2022-05-27 19:33:17 -0300

    Removed cache import to use the cache recommended by Microsoft & added cache experiments in DemonsController

--- ShinCacheTensei/Controllers/DemonsController.cs
+++ ShinCacheTensei/Controllers/DemonsController.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+//using System.Runtime.Caching;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace ShinCacheTensei.Controllers
 {
@@ -11,10 +13,16 @@ namespace ShinCacheTensei.Controllers
     [Route("[controller]")]
     public class DemonsController : ControllerBase
     {
+        private readonly MemoryCache _MemoryCache;
+        public DemonsController(MemoryCache memoryCache) {
+            _MemoryCache.CreateEntry(4).Value = "Valor";
+            _MemoryCache = memoryCache;
+        }
 
         [HttpGet]
         public IEnumerable<int> GetSomething()
         {
+            _MemoryCache.TryGetValue(4, out var result);
             return Enumerable.Range(0, 5);
         }
     }
