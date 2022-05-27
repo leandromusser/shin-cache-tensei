d7a54efe7e72ac6e4cfd52959c6f57ba111d593a
Leandro Rocha <leandro.musser@gmail.com>
2022-05-27 03:17:39 -0300

    Adicionar arquivos de projeto.

---
+++ ShinCacheTensei/Models/CommonEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace ShinCacheTensei.Models
+{
+    public class CommonEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ShinCacheTensei/Models/RecruitingMethod.cs
@@ -0,0 +1,8 @@
+﻿namespace ShinCacheTensei.Models
+{
+    public class RecruitingMethod
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+    }
+}
