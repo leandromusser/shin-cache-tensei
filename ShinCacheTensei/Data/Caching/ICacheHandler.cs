e6561dcee7c8402ab6f3c8ff6c1e2f132d102798
Leandro Musser <leandro.musser@gmail.com>
2022-05-30 18:51:38 -0300

    Criação de uma classe de cache para todas as entidades em vez de 1 para Demon, outra para Skill, etc. Em construção.

---
+++ ShinCacheTensei/Data/Caching/ICacheHandler.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using ShinCacheTensei.Entities;
+
+namespace ShinCacheTensei.Data.Caching
+{
+    public interface ICacheHandler
+    {
+
+        //public TryObtainDemonWithName(object key, out Demon demon)
+        public bool TryGetValue(object key, out object value);
+        public bool Add(object key, object value);
+
+    }
+}
