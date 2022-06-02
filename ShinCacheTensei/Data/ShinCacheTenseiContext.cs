a8d13dd97e5597fdadbf338824a559befb576d34
Leandro Musser <leandro.musser@gmail.com>
2022-06-02 03:18:51 -0300

    Added remaining entites to the Context class + overrided OnModelCreating method to do some adjusts later

--- ShinCacheTensei/Data/ShinCacheTenseiContext.cs
+++ ShinCacheTensei/Data/ShinCacheTenseiContext.cs
@@ -6,11 +6,23 @@ namespace ShinCacheTensei.Data
 {
     public class ShinCacheTenseiContext: DbContext
     {
+        public DbSet<AffinityType> AffinityTypes { get; set; }
         public DbSet<Demon> Demons { get; set; }
+        public DbSet<DemonAffinity> DemonAffinities { get; set; }
+        public DbSet<DemonSkill> DemonSkills { get; set; }
+        public DbSet<Nature> Natures { get; set; }
+        public DbSet<RecruitingMethod> RecruitingMethods { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<SkillCost> SkillCosts { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(
                 @"Server=(localdb)\mssqllocaldb;Database=Blogging;Trusted_Connection=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            //modelBuilder
         }
     }
 }
