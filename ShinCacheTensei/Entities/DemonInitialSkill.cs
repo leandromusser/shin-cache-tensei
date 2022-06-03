9a54e5f7a66a7c7620dbf3e91abc175b9dbceecc
Leandro Musser <leandro.musser@gmail.com>
2022-06-03 02:20:34 -0300

    Renamed class to make the name more intuitive

---
+++ ShinCacheTensei/Entities/DemonInitialSkill.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShinCacheTensei.Entities
+{
+    public class DemonInitialSkill
+    {
+        public int DemonId { get; set; }
+        public int SkillId { get; set; }
+        public Demon Demon { get; set; }
+        public int UnlockLevel { get; set; }
+        public Skill Skill { get; set; }
+    }
+}
--- ShinCacheTensei/Entities/DemonSkill.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ShinCacheTensei.Entities
-{
-    public class DemonSkill
-    {
-        public Demon Demon { get; set; }
-        public int UnlockLevel { get; set; }
-        public Skill Skill { get; set; }
-    }
-}
