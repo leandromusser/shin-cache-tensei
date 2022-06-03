708ac90bdc69734335eeae9d72d64a019f0470d9
Leandro Musser <leandro.musser@gmail.com>
2022-06-03 02:23:21 -0300

    Used Entity Framework Core's Fluent API to set composite Primary Keys

--- ShinCacheTensei/Data/ShinCacheTenseiContext.cs
+++ ShinCacheTensei/Data/ShinCacheTenseiContext.cs
@@ -9,7 +9,7 @@ namespace ShinCacheTensei.Data
         public DbSet<AffinityType> AffinityTypes { get; set; }
         public DbSet<Demon> Demons { get; set; }
         public DbSet<DemonAffinity> DemonAffinities { get; set; }
-        public DbSet<DemonSkill> DemonSkills { get; set; }
+        public DbSet<DemonInitialSkill> DemonInitialSkills { get; set; }
         public DbSet<Nature> Natures { get; set; }
         public DbSet<RecruitingMethod> RecruitingMethods { get; set; }
         public DbSet<Skill> Skills { get; set; }
@@ -22,7 +22,29 @@ namespace ShinCacheTensei.Data
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
-            //modelBuilder
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<DemonInitialSkill>().HasKey(l => new { l.DemonId, l.SkillId});
+            modelBuilder.Entity<DemonAffinity>().HasKey(l => new { l.DemonId, l.NatureId, l.AffinityTypeId });
+
+            //modelBuilder.Entity<DemonAffinity>().HasKey(k => k.Demon.Id);
+
+            //modelBuilder.Entity<DemonAffinity>().OwnsOne<Demon>(x => x.Demon);
+            //modelBuilder.Entity<DemonInitialSkill>().OwnsOne<Demon>(x => x.Demon).HasKey()
+
+            //modelBuilder.Entity<Demon>().Property(p => p.DemonInitialSkills).
+
+            //modelBuilder.Entity<Demon>().HasOne<Demon>(x => x).WithMany<DemonAffinity>(x => x.STR);
+
+            //modelBuilder.Entity<DemonInitialSkill>().HasKey(k => k.Demon.Id);
+
+            //modelBuilder.Entity<DemonAffinity>().HasOne<Demon>(x => x.Demon).WithMany(x => x.Id);
+
+            //modelBuilder.Entity<DemonAffinity>().HasKey(x => x.DemonId).
+
+            //Pesquisar por Value Object / Objeto de valor
+            //O Entity Framework não suporta tipos complexos, por isso vai dar erro aqui com o k.Demon.Id. Teria que ser tipo um K.DemonId
+            //modelBuilder.Entity<DemonInitialSkill>().HasKey(k => k.Demon.Id);
         }
     }
 }
