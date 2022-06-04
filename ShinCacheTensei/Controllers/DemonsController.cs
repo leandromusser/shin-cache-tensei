5483d3ec8554ff1de1cd5743c1f4dcfc1cdfc8a4
Leandro Musser <leandro.musser@gmail.com>
2022-06-04 16:13:12 -0300

    Added basic validation with data annotations inside the new DemonQueryParams class

--- ShinCacheTensei/Controllers/DemonsController.cs
+++ ShinCacheTensei/Controllers/DemonsController.cs
@@ -21,23 +21,7 @@ namespace ShinCacheTensei.Controllers
     {
         private readonly IDemonService _demonService;
         public DemonsController(IDemonService demonService) {
-            //MemoryCache.CreateEntry(4).Value = "Valor";
             _demonService = demonService;
-        }
-
-        [HttpGet]
-        [Route("searchC")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [SwaggerOperation(Summary = "APENAS TESTE: Busca no cache por todos os demons com os ids passados. Caso não sejam encontrados lá, são criados e encontrados em " +
-            "chamadas posteriores.")]
-        public IActionResult GetFromCacheTestEndPoint([FromQuery(Name = "id")] int[] ids)
-        {
-            _demonService.GetById(ids[0], out Demon demon);
-            return Ok(demon);
-            if (_demonService.GetByIds(ids, out IEnumerable<DemonDto> demonDtos))
-                return Ok(demonDtos);
-            return NotFound();
         }
 
         [HttpGet]
@@ -45,13 +29,14 @@ namespace ShinCacheTensei.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [SwaggerOperation(Summary = "Retorna todas as informações dos Demons com os ids solicitados.")]
-        public IActionResult GetDemons([FromQuery(Name = "id")] int[] ids)
+        public IActionResult GetDemons([FromQuery] DemonQueryParams demonQueryParams)
         {
-            if (_demonService.GetByIds(ids, out IEnumerable<DemonDto> demonDtos))
+            if (!this.ModelState.IsValid)
+                return BadRequest("Invalid parameters!");
+
+            if (_demonService.GetByIds(demonQueryParams.Ids, out IEnumerable<DemonDto> demonDtos))
                 return Ok(demonDtos);
             return NotFound();
         }
-
-
     }
 }
---
+++ ShinCacheTensei/Data/DemonQueryParams.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShinCacheTensei.Data
+{
+    public class DemonQueryParams
+    {
+        [Required]
+        [MinLength(1, ErrorMessage = "Number of ids cannot be below 1.")]
+        [MaxLength(5, ErrorMessage = "Number of ids cannot exceed 5.")]
+        [FromQuery(Name = "id")]
+        public int[] Ids { get; set; }
+    }
+}
