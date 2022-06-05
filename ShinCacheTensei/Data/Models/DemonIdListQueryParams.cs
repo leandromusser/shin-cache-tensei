6e6525e526290a592f0423adc44b296b886ce5c8
Leandro Musser <leandro.musser@gmail.com>
2022-06-05 16:22:25 -0300

    Added model for searching demons by id and using with filters

---
+++ ShinCacheTensei/Data/Models/DemonIdListQueryParams.cs
@@ -0,0 +1,16 @@
+﻿using ShinCacheTensei.Entities;
+
+namespace ShinCacheTensei.Data.Models
+{
+    public class DemonIdListQueryParams
+    {
+        public int Resistance { get; set; }
+        public int Weakness { get; set; }
+        public int MinimumLevel { get; set; }
+        public int MaximumLevel { get; set; }
+        public int DemonRace { get; set; }
+        public int Skill { get; set; }
+        public string Name { get; set; }
+        public int Index { get; set; }
+    }
+}
