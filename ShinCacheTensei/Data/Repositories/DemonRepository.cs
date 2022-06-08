5bc557116252a93ca18f867f078d43a59abc87c3
Leandro Musser <leandro.musser@gmail.com>
2022-06-08 02:41:24 -0300

    Fixed pagination in DemonRepository and renamed a variable

--- ShinCacheTensei/Data/Repositories/DemonRepository.cs
+++ ShinCacheTensei/Data/Repositories/DemonRepository.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Collections.Generic;
 using ShinCacheTensei.Data.Models;
 using Microsoft.Extensions.Configuration;
+using System;
 
 namespace ShinCacheTensei.Data.Repositories
 { 
@@ -23,36 +24,41 @@ namespace ShinCacheTensei.Data.Repositories
         public bool GetIdsByFilters(DemonIdListQueryParams demonIdListQueryParams, int quantity, out int[] ids)
         {
 
-            IQueryable<Demon> queryIdsss =
+            IQueryable<Demon> query =
                 from Demon in _shinCacheTenseiContext.Demons.Include(d => d.DemonInitialSkills)
                     .Include(d => d.DemonAffinities).Include(d => d.DemonAffinities).Include(d => d.Race)
                 select Demon;
 
             if (demonIdListQueryParams.ResistNatureId != null)
-                queryIdsss = queryIdsss.Where(d => d.DemonAffinities.Any(a => a.AffinityType.Name.Equals("Weak") && a.NatureId == demonIdListQueryParams.ResistNatureId));
+                query = query.Where(d => d.DemonAffinities.Any(a => a.AffinityType.Name.Equals("Weak") && a.NatureId == demonIdListQueryParams.ResistNatureId));
 
             if (demonIdListQueryParams.WeakNatureId != null)
-                queryIdsss = queryIdsss.Where(d => d.DemonAffinities.Any(a => !a.AffinityType.Name.Equals("Weak") && a.NatureId == demonIdListQueryParams.WeakNatureId));
+                query = query.Where(d => d.DemonAffinities.Any(a => !a.AffinityType.Name.Equals("Weak") && a.NatureId == demonIdListQueryParams.WeakNatureId));
 
             if (demonIdListQueryParams.MinimumLevel != null)
-                queryIdsss = queryIdsss.Where(d => d.InitialLevel >= demonIdListQueryParams.MinimumLevel);
+                query = query.Where(d => d.InitialLevel >= demonIdListQueryParams.MinimumLevel);
 
             if (demonIdListQueryParams.MaximumLevel != null)
-                queryIdsss = queryIdsss.Where(d => d.InitialLevel <= demonIdListQueryParams.MaximumLevel);
+                query = query.Where(d => d.InitialLevel <= demonIdListQueryParams.MaximumLevel);
 
             if (demonIdListQueryParams.DemonRaceId != null)
-                queryIdsss = queryIdsss.Where(d => d.Race.Id == demonIdListQueryParams.DemonRaceId);
+                query = query.Where(d => d.Race.Id == demonIdListQueryParams.DemonRaceId);
 
             if (demonIdListQueryParams.SkillId != null)
-                queryIdsss = queryIdsss.Where(d => d.DemonInitialSkills.Any(s => s.SkillId == demonIdListQueryParams.SkillId));
+                query = query.Where(d => d.DemonInitialSkills.Any(s => s.SkillId == demonIdListQueryParams.SkillId));
 
             if (demonIdListQueryParams.ContainsThisTextInName != null)
-                queryIdsss = queryIdsss.Where(d => d.Name.ToLower().Contains(demonIdListQueryParams.ContainsThisTextInName.ToLower()));
+                query = query.Where(d => d.Name.ToLower().Contains(demonIdListQueryParams.ContainsThisTextInName.ToLower()));
 
             if (demonIdListQueryParams.AfterId != null)
-                queryIdsss = queryIdsss.Where(d => demonIdListQueryParams.AfterId < d.Id);
+            {
+                //Infelizmente, ao sair do IQueryable, todos os dados da query são puxados e filtrados aqui mesmo no servidor
+                //Registro que tenho ciência deste problema
+                ids = query.ToArray().SkipWhile(s => s.Id != demonIdListQueryParams.AfterId).Skip(1).Take(quantity).Select(d => d.Id).ToArray();
+            }
+            else
+                ids = query.Take(quantity).Select(d => d.Id).ToArray();
 
-            ids = queryIdsss.Select(d => d.Id).Take(quantity).ToArray();
             return ids.Any();
 
         }
