bd69df2cd40745e7f5a4b58826a653f240cce0c7
Leandro Musser <leandro.musser@gmail.com>
2022-06-08 00:25:22 -0300

    Added JsonIgnore Attribute in some fields to control what is being returned by the controller

--- ShinCacheTensei/Entities/DemonAffinity.cs
+++ ShinCacheTensei/Entities/DemonAffinity.cs
@@ -1,17 +1,22 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace ShinCacheTensei.Entities
 {
     ///Cada Demon pode ter fraquezas e forças de várias categorias. Um demon [Demon] pode absorver [AffinityType], por exemplo, Fire, Ice e Light [Nature].
     public class DemonAffinity
     {
+        [JsonIgnore]
         public int DemonId { get; set; }
 
+        [JsonIgnore]
         public int NatureId { get; set; }
 
+        [JsonIgnore]
         public int AffinityTypeId { get; set; }
 
+        [JsonIgnore]
         public Demon Demon  { get; set; }
 
         public Nature Nature { get; set; }
--- ShinCacheTensei/Entities/DemonInitialSkill.cs
+++ ShinCacheTensei/Entities/DemonInitialSkill.cs
@@ -1,12 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace ShinCacheTensei.Entities
 {
     public class DemonInitialSkill
     {
+        [JsonIgnore]
         public int DemonId { get; set; }
+        [JsonIgnore]
         public int SkillId { get; set; }
+        [JsonIgnore]
         public Demon Demon { get; set; }
         public int UnlockLevel { get; set; }
         public Skill Skill { get; set; }
