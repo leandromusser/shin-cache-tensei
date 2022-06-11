81c64e31b226a3f4169d8c2ab6c5474f644cd81f
Leandro Musser <leandro.musser@gmail.com>
2022-06-11 16:08:26 -0300

    Refactored method in FilterOptionsService class

--- ShinCacheTensei/Services/Implementations/FilterOptionsService.cs
+++ ShinCacheTensei/Services/Implementations/FilterOptionsService.cs
@@ -24,28 +24,24 @@ namespace ShinCacheTensei.Services
 
         private delegate IEnumerable<Tuple<int, string>> GetFiltersPerCategoryMethod();
 
-        private IEnumerable<FilterTotalOptionsDto> GetAvailableFilterOptions(Tuple<string, GetFiltersPerCategoryMethod>[] CategoryNameAndValues) {
+        public IEnumerable<FilterTotalOptionsDto> GetAvailableFilterOptions() {
 
             var filterOptionsDtos = new List<FilterOptionDto>();
             var filterTotalOptionsDtos = new List<FilterTotalOptionsDto>();
 
-            foreach (var categoryNameAndValues in CategoryNameAndValues) {
-                foreach (var tupleIdsAndValues in categoryNameAndValues.Item2.Invoke()) {
+            foreach (var categoryNameAndValues in new List<Tuple<string, GetFiltersPerCategoryMethod>>() {
+                Tuple.Create<string, GetFiltersPerCategoryMethod>("Nature", _filterOptionRepository.GetNatureFilterOptions),
+                Tuple.Create<string, GetFiltersPerCategoryMethod>("DemonRace", _filterOptionRepository.GetDemonRaceFilterOptions),
+                Tuple.Create<string, GetFiltersPerCategoryMethod>("Skill", _filterOptionRepository.GetSkillFilterOptions)
+            }.ToArray()) {
+                foreach (var tupleIdsAndValues in categoryNameAndValues.Item2.Invoke())
+                {
                     filterOptionsDtos.Add(new FilterOptionDto(tupleIdsAndValues.Item2, tupleIdsAndValues.Item1, OriginType.Database));
                 }
                 filterTotalOptionsDtos.Add(new FilterTotalOptionsDto(filterOptionsDtos, categoryNameAndValues.Item1));
                 filterOptionsDtos = new List<FilterOptionDto>();
             }
             return filterTotalOptionsDtos;
-        }
-
-        public IEnumerable<FilterTotalOptionsDto> GetAvailableFilterOptions() {
-
-            return GetAvailableFilterOptions(new List<Tuple<string, GetFiltersPerCategoryMethod>>() {
-                Tuple.Create<string, GetFiltersPerCategoryMethod>("Nature", _filterOptionRepository.GetNatureFilterOptions),
-                Tuple.Create<string, GetFiltersPerCategoryMethod>("DemonRace", _filterOptionRepository.GetDemonRaceFilterOptions),
-                Tuple.Create<string, GetFiltersPerCategoryMethod>("Skill", _filterOptionRepository.GetSkillFilterOptions)
-            }.ToArray());
         }
     }
 }
