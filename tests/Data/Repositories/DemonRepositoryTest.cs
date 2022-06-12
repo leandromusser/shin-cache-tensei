8f3061fdbd60950bd70884bfe4010ab92c516e9c
Leandro Musser <leandro.musser@gmail.com>
2022-06-12 14:11:16 -0300

    Added empty NUnit structure

---
+++ tests/Data/Repositories/DemonRepositoryTest.cs
@@ -0,0 +1,24 @@
+﻿using ShinCacheTensei.Data.Models;
+using ShinCacheTensei.Data.Repositories;
+using ShinCacheTensei.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tests.Data.Repositories
+{
+    internal class DemonRepositoryTest
+    {
+        public bool GetByIds(int[] ids, out IEnumerable<Demon> demons)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GetIdsByFilters(DemonIdListQueryParams demonIdListQueryParams, int quantity, out int[] ids)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
