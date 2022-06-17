46d8e1ad2a5325227133bda82c8d67c7c585986c
Leandro Musser <leandro.musser@gmail.com>
2022-06-17 01:37:47 -0300

    Added temporary working test code to test DemonRepository

--- tests/Data/Repositories/DemonRepositoryTest.cs
+++ tests/Data/Repositories/DemonRepositoryTest.cs
@@ -1,4 +1,8 @@
-﻿using ShinCacheTensei.Data.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using ShinCacheTensei.Data;
+using ShinCacheTensei.Data.Models;
 using ShinCacheTensei.Data.Repositories;
 using ShinCacheTensei.Entities;
 using System;
@@ -9,16 +13,42 @@ using System.Threading.Tasks;
 
 namespace tests.Data.Repositories
 {
-    internal class DemonRepositoryTest
+    [TestFixture]
+    public class DemonRepositoryTest
     {
-        public bool GetByIds(int[] ids, out IEnumerable<Demon> demons)
+        //Trocar o nome do banco de dados depois para tirar esse que é padrão
+        private readonly DbContextOptions<ShinCacheTenseiContext> dbContextOptions = new DbContextOptionsBuilder<ShinCacheTenseiContext>()
+        .UseInMemoryDatabase(databaseName: "Blogging")
+        .Options;
+        private ShinCacheTenseiContext _shinCacheTenseiContext;
+        private DemonRepository _demonRepository;
+
+        [OneTimeSetUp]
+        public void Setup()
         {
-            throw new NotImplementedException();
+            /*
+            //Vai ser usados nos testes do CacheRepository
+            IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath("/")
+                .AddInMemoryCollection(new Dictionary<string, string>())
+                .Build();
+            */
+
+            _shinCacheTenseiContext = new ShinCacheTenseiContext(dbContextOptions);
+            AddData();
+            _demonRepository = new DemonRepository(_shinCacheTenseiContext);
         }
 
-        public bool GetIdsByFilters(DemonIdListQueryParams demonIdListQueryParams, int quantity, out int[] ids)
-        {
-            throw new NotImplementedException();
+        private void AddData() {
+            _shinCacheTenseiContext.RecruitingMethods.Add(new RecruitingMethod() { Id = 1, Description = "Fusion only" });
+            _shinCacheTenseiContext.DemonRaces.Add(new DemonRace() { Id = 1, Name = "Avatar" });
+            _shinCacheTenseiContext.Demons.Add(new Demon() { Id = 5, Name = "Leandro", InitialLevel = 50, DemonRaceId = 1, RecruitingMethodId = 1});
+            _shinCacheTenseiContext.SaveChanges();
         }
+
+        [Test]
+        public void FirstTest() {
+            Assert.That(_demonRepository.GetByIds(new int[] {5}).Result, Has.Count.AtLeast(1));
+        }
+        
     }
 }
